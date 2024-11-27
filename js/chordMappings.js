fb5a0c501f00b578446bf72c803dd428dabfec81
Seth Asafo <sethasafogh@gmail.com>
2024-11-27 15:22:28 +0000

    add chord mapping script

---
+++ js/chordMappings.js
@@ -0,0 +1,15 @@
+// Chords for each key
+export const chords = {
+    1: ["C", "E", "G"],  // C major chord
+    2: ["D", "F#", "A"], // D major chord
+    3: ["E", "G#", "B"], // E major chord
+    // Add all chord mappings...
+};
+
+// Portals for switching key mappings
+export const portals = {
+    1: chords, // Portal for C major
+    2: chords, // Portal for D major
+    3: chords, // Portal for E major
+    // Add all portal mappings...
+};
