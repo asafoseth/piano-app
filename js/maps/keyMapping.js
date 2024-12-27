069be495eb013f0f7cb554cd7bc321ef380922f1
Seth Asafo <sethasafogh@gmail.com>
2024-12-27 22:09:47 +0000

    add all maps into the map folder

---
+++ js/maps/keyMapping.js
@@ -0,0 +1,9 @@
+    // Map physical keys to piano keys, including 12 invisible bass keys
+    export const keyMap = {
+        q: 1, w: 2, e: 3, r: 4, t: 5, y: 6, u: 7, i: 8, o: 9, p: 10,
+        a: 11, s: 12, d: 13, f: 14, g: 15, h: 16, j: 17, k: 18, l: 19,
+        z: 20, x: 21, c: 22, v: 23, b: 24,
+        // Invisible bass keys
+        '1': 25, '2': 26, '3': 27, '4': 28, '5': 29, '6': 30,
+        '7': 31, '8': 32, '9': 33, '0': 34, '-': 35, '=': 36
+    };
